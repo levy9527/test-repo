003d80450a543aa55d2c89715c96ba40e5787132
levy <levy9527@users.noreply.github.com>
2019-05-10 17:24:43 +0800

    feat: use build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yarn build
+
+git config --global user.email "897895407@qq.com"
+git config --global user.name "levy9527"
+
+git remote add github https://$GITHUB_TOKEN@github.com/levy9527/test-repo.git > /dev/null 2>&1
+git push github HEAD:dev
+git push github HEAD:master --follow-tags
+
