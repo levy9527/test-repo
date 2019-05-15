52b45099c89518a24c2843bd25207ff18dd8673c
levy <897895407@qq.com>
2019-05-15 13:53:16 +0800

    chore: optimize release and notify

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ yarn stdver
 git remote add github https://$GITHUB_TOKEN@github.com/levy9527/test-repo.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
+
