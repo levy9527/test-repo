0072984dae61cd614e00d43cb4c510ef4fad5407
levy <levy9527@users.noreply.github.com>
2020-07-23 14:15:41 +0800

    chore: notify (#27)

--- build.sh
+++ build.sh
@@ -2,9 +2,3 @@
 yarn stdver
 
 yarn build
-
-git remote add github https://$GITHUB_TOKEN@github.com/levy9527/test-repo.git > /dev/null 2>&1
-git push github HEAD:master --follow-tags
-
-GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
-
